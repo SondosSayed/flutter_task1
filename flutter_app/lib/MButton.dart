41bc6037d03177bbaeaa8a75be60262db5411151
SondosSayed <110243533+SondosSayed@users.noreply.github.com>
2024-09-20 19:41:22 +0300

    Update MButton.dart

--- flutter_app/lib/MButton.dart
+++ flutter_app/lib/MButton.dart
@@ -1,21 +1,16 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-
 class MButton extends StatelessWidget{
   final String text1;
-
   const MButton({super.key,required this.text1});
-
   @override
   Widget build(BuildContext context) {
-
     return MaterialButton(
       onPressed: () {},
       height: 50,
       color: Colors.grey,
       shape: RoundedRectangleBorder(
         borderRadius: BorderRadius.circular(30),
-
       ),
       // decoration: BoxDecoration(
       // ),
@@ -23,10 +18,6 @@ class MButton extends StatelessWidget{
         child: Text(text1, style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),),
       ),
     );
-
-
   }
-
-
 
 }
